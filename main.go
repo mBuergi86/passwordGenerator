62e031290a3d808664d97ef612d49ea5e4d0c109
mbuergi <m.buergi@linguala.com>
2024-09-07 18:06:20 +0200

    feat: ✨ Update main.go with the following arguments

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	// "flag"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"strings"
 )
 
@@ -36,6 +39,52 @@ func passwordGenerate(length int, isDigit bool, isSpecial bool, isLowercase bool
 }
 
 func main() {
-	length := 12
-	fmt.Print(passwordGenerate(length, true, true, true, true))
+	//	length := flag.Int("length", 8, "Password length")
+	//	isDigit := flag.Bool("digit", true, "Include digits")
+	//	isSpecial := flag.Bool("special", true, "Include special characters")
+	//	isLowercase := flag.Bool("lowercase", true, "Include lowercase characters")
+	//	isUppercase := flag.Bool("uppercase", true, "Include uppercase characters")
+
+	//	flag.Parse()
+
+	if len(os.Args) != 6 {
+		fmt.Println("Usage: password-generator <length> <isDigit> <isSpecial> <isLowercase> <isUppercase>")
+		fmt.Println("Example: password-generator 8 true true true true")
+		os.Exit(1)
+	}
+
+	length, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Invalid length")
+		os.Exit(1)
+	}
+
+	isDigit, err := strconv.ParseBool(os.Args[2])
+	if err != nil {
+		fmt.Println("Invalid isDigit")
+		os.Exit(1)
+	}
+
+	isSpecial, err := strconv.ParseBool(os.Args[3])
+	if err != nil {
+		fmt.Println("Invalid isSpecial")
+		os.Exit(1)
+	}
+
+	isLowercase, err := strconv.ParseBool(os.Args[4])
+	if err != nil {
+		fmt.Println("Invalid isLowercase")
+		os.Exit(1)
+	}
+
+	isUppercase, err := strconv.ParseBool(os.Args[5])
+	if err != nil {
+		fmt.Println("Invalid isUppercase")
+		os.Exit(1)
+	}
+
+	// password := passwordGenerate(*length, *isDigit, *isSpecial, *isLowercase, *isUppercase)
+	password := passwordGenerate(length, isDigit, isSpecial, isLowercase, isUppercase)
+
+	fmt.Printf("Generated Password: %s\n", password)
 }
